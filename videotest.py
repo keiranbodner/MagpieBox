69c84dd489c89a253a188b48a92c32c33226a6fb
keiranbodner <108036169+keiranbodner@users.noreply.github.com>
2022-06-24 17:44:33 -0600

    Update videotest.py

--- videotest.py
+++ videotest.py
@@ -7,6 +7,7 @@ camera.resolution = (1280,720)
 camera.framerate = 25
 
 time.sleep(3)
+moment = datetime.now()
 camera.start_recording('/home/uleth/Videos/video_%02d_%02d_%02d.mjpg' % (moment.hour, moment.minute, moment.second))
 sleep(10)
 camera.stop_recording()
