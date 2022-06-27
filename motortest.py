e79873b63d63c942c05d73040d494ede427b4070
keiranbodner <108036169+keiranbodner@users.noreply.github.com>
2022-06-27 11:18:13 -0600

    Update motortest.py

--- motortest.py
+++ motortest.py
@@ -1,25 +1,25 @@
 import RPi.GPIO as GPIO
-
-BEAM_OFF= 21
-BEAM_ON= 20
-
-def break_beam_callback(channel):
-    if GPIO.input(BEAM_OFF):
-        print("beam1 unbroken")
-    else:
-        print("beam1 broken")
-
-def break_beam_callback1(channel):
-    if GPIO.input(BEAM_ON):
-        print("beam2 unbroken")
-    else:
-        print("beam2 broken")
+from time import sleep
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(BEAM_OFF, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.add_event_detect(BEAM_OFF, GPIO.BOTH, callback=break_beam_callback)
-GPIO.setup(BEAM_ON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.add_event_detect(BEAM_ON, GPIO.BOTH, callback=break_beam_callback1)
+GPIO.setwarnings(False)
 
-message = input("Press enter to quit\n\n")
-GPIO.cleanup()
+En,In1,In2 = 23,24,25
+GPIO.setup(En,GPIO.OUT)
+GPIO.setup(In1,GPIO.OUT)
+GPIO.setup(In2,GPIO.OUT)
+pwm = GPIO.PWM(En, 100)
+pwm.start(0)
+
+variable = [1,2,3,4,5]
+
+
+while True:
+    GPIO.output(In1, GPIO.LOW)
+    GPIO.output(In2, GPIO.HIGH)
+    pwm.ChangeDutyCycle(50)
+    sleep(5)
+    GPIO.output(In1, GPIO.LOW)
+    GPIO.output(In2, GPIO.HIGH)
+    pwm.ChangeDutyCycle(100)
+    sleep(5)
